8ea5fc3f1d3b633f2f08ee300914a3af147e59c7
patribriyus <patribriyus@hotmail.com>
2020-09-30 13:26:54 +0200

    p1

---
+++ p1/p1.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : p1.cpp
+// Author      : 735576,
+// Version     : 1
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!");
+	return EXIT_SUCCESS;
+}
