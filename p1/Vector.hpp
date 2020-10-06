32d8badd33abfc150d53623998dd39e212765ade
Diego Caballé <diegocaballe@hotmail.com>
2020-10-06 18:23:44 +0200

    más operaciones en vector

--- p1/Vector.hpp
+++ p1/Vector.hpp
@@ -21,6 +21,11 @@ public:
         return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
     }
 
+    Vector productoVectorial(Vector a, Vector b){
+
+        return {a.y * b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x};
+    }
+
     Vector operator+(const Vector b) {
         return {this->x + b.x, this->y + b.y, this->z + b.z};
     }
@@ -29,8 +34,8 @@ public:
         return {this->x - b.x, this->y - b.y, this->z - b.z};
     }
 
-    Vector operator*(const Vector b) {
-        return {this->x * b.x, this->y * b.y, this->z * b.z};
+    double operator*(const Vector b) {  //producto escalar
+        return this->x * b.x + this->y * b.y + this->z * b.z;
     }
 
     Vector operator/(const Vector b) {
