3b6cd8368bd0aa4783d9ea99c0cbe8ec2411b71d
Diego Caballé <diegocaballe@hotmail.com>
2020-11-09 01:19:13 +0100

    primera escena comentada en el main. generación de rayos etc en el main directamente, si quieres en alguna función

--- p3/main.cpp
+++ p3/main.cpp
@@ -2,3 +2,27 @@
 // Created by diego on 7/11/20.
 //
 
+#include <string>
+#include "Plane.hpp"
+#include "Camera.hpp"
+#include "Sphere.hpp"
+
+
+int main(int argc, char* argv[]){
+
+    std::string s = argv[1];
+    int pixelRes = stoi(s);
+
+    //primera escena
+    //
+    //Esfera 0,0,10 / 10
+    //Cámara 0,0,0 / y = 0,15,0 – z = 0,0,10 – x = y X z
+    //Pixeles 144
+
+    Sist_coord cam = Sist_coord(Vector(0,0,10),Vector(0,10,0));
+
+    Camera camera = Camera(cam, Punto(0,0,0));
+
+    camera.
+
+}
