4a76e818e3c87913d855e60fed7aa8f9d227f412
Diego Caballé <diegocaballe@hotmail.com>
2020-11-13 11:05:23 +0100

    rgb y escena del profesor

--- p3/main.cpp
+++ p3/main.cpp
@@ -9,7 +9,14 @@
 #include "Sphere.hpp"
 #include "../p1/Matriz.hpp"
 
+const int Y =  1;
+const int X =  1;
 
+struct rgb{
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+};
 
 int main(int argc, char* argv[]){
 
@@ -23,7 +30,7 @@ int main(int argc, char* argv[]){
 
     ldrfile << "P3" << endl;
     ldrfile << "#MAX=255" << endl;
-    ldrfile << "15 15" << endl;
+    ldrfile << "2 2" << endl;
     ldrfile << "255" << endl;
 
     //primera escena
@@ -33,26 +40,28 @@ int main(int argc, char* argv[]){
     //Pixeles 144
 
     //Escena
-    Sphere shape1 = Sphere(Punto(0,0,10), 10.0, 255, 0,0);
+    Sphere shape1 = Sphere(Punto(0,0,0), 1, 255, 0,0);
 
     //SistCoordenadasCam
     Vector z = Vector(0,0,10);
-    Vector y = Vector(0,15,0);
-    Vector x = Vector(15,0,0);
-    Punto center = Punto(0,0,0);
+    Vector y = Vector(0,Y,0);
+    Vector x = Vector(X,0,0);;
+    Punto center = Punto(0,0,-10);
     //Sistemas de coordenadas en matriz para hacer el cambio de sistemas
     Matriz siscam = Matriz(x,y,z,center);
 
     //plano de proyección
-    double area = 15*15;
+    double area = (X*2)*(Y*2);
     double pixelUnit = area / (double)pixelRes;
+
     //variables para el for
     Rayo r;
     double xLocal, yLocal, max = -1;
-    int rmax = 0,gmax = 0,bmax = 0;
-    Vector dirLocal, dirGlobal;
-    //cuantos pixeles tendrá cada lado, si hay 3x3 pixeles iteraremos para sacar los 9 rayos en cada pixel
-    int howManyPixelsOnSide = 15.0 / pixelUnit;
+    rgb rgbmax;
+    Vector dirLocal;
+
+    //cuantos pixeles tendrá cada lado, se asume cuadrado
+    int howManyPixelsOnSide = X*2 / pixelUnit;
 
     for( int f = 1; f <= howManyPixelsOnSide; f++){
         for(int c = 1; c <= howManyPixelsOnSide; c++){
@@ -65,23 +74,24 @@ int main(int argc, char* argv[]){
             Matriz local = Matriz(dirLocal, 0);
 
             //cambio de base, de salida tendremos la dirección del vector en coordenadas globales
-            Matriz Global = siscam * local;
+            Matriz dirGlobal = siscam * local;
 
-            r = Rayo(center, Global.vector());
+            r = Rayo(center, dirGlobal.vector());
             //aquí usariamos este rayo para iterar sobre la lista de figuras
             //como sólo tenemos una...
             double res = r.interseccion(shape1);
             if(res > 0 && res > max){
                 max = res;
-                rmax = shape1.getRed();
-                gmax = shape1.getGreen();
-                bmax = shape1.getBlue();
+                rgbmax.red = shape1.getRed();
+                rgbmax.green = shape1.getGreen();
+                rgbmax.blue = shape1.getBlue();
             }
             //escribimos en el fichero
-            ldrfile << rmax << " " << gmax << " " << bmax << " ";
-            rmax = 0;
-            gmax = 0;
-            bmax = 0;
+            ldrfile << rgbmax.red << " " << rgbmax.green << " " << rgbmax.blue << " ";
+
+            rgbmax.red = 0;
+            rgbmax.green = 0;
+            rgbmax.blue = 0;
             max = -1;
         }
     }
