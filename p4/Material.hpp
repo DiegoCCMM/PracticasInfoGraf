93a619c188d49d875ccc4c4250e177113fd5c1e0
Diego Caballé <diegocaballe@hotmail.com>
2021-01-26 15:59:24 +0100

    PATRI CURRA YA E

--- p4/Material.hpp
+++ p4/Material.hpp
@@ -2,6 +2,10 @@
 // Created by diego on 23/1/21.
 //
 
+#include <random>
+#include <cmath>
+#include "Camera.hpp"
+
 #ifndef P4_MATERIAL_HPP
 #define P4_MATERIAL_HPP
 
@@ -9,12 +13,23 @@ class Material{
 
     double kd, ks, kt;
 
-    void rayosSecundarios(){
+    void rayosSecundarios(Rayo rayo, int r, int g, int b, Vector normal){
         //TODO fr(x, wi, w0) = kd/pi + ks(x, w0)(delta wr(wi) / n*wi) + kt(x,w0)(delta wt(wi)/n*wi)
-        //TODO delta wr = 2n(n*wi)-wi
+        //TODO delta wr = 2n(n*wi) - wi
         //TODO delta wt = arcsin((n0 * sin(w0)) / n1)
         //TODO https://es.wikipedia.org/wiki/%C3%8Dndice_de_refracci%C3%B3n
         //TODO vidrio 1,45 aire 1
+    }
+    Vector muestreoCoseno(){
+        srand(NULL);
+
+        int Einclination, Eazimuth
+
+        Einclination = rand();
+        Eazimuth = rand();
+
+        double inclinationi = acos(sqrt(1-Einclination));
+        double azimuthi = 2*M_PI*Eazimuth
     }
 
 };
